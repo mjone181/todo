0dea037cafdcf434119d3c03f08375a4e0d72bed
mjone181 <mjone181@gmail.com>
2020-12-01 15:27:34 -0600

    Added State Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  //Above we have created a class called App that extends the functionality of the Component class.
 
-export default App;
+  //The export keyword above makes the class available for use outside of the JS file where it is created
+
+  constructor(){
+    super();
+    //  React components have a special property called "state".  The "state" is used to define the state of data (props)
+    this.state = {
+      todoOwner: "Shawn",
+      todoList: [
+        {action: "Move burn pile", done: false},
+        {action: "Oil Change", done: false},
+        {action: "Start x-mas shopping", done: false},
+        {action: "Pay November Sales Tax", done: false},
+        {action: "Eat breakfast", done: true}
+      ]     
+    }
+
+  }//END OF CONSTRUCTOR
+}//END OF APP
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
